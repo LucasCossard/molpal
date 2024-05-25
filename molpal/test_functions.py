8ca47520cb28dbca5d7075815791d69c432e2a74
Nohalyan <160868437+Nohalyan@users.noreply.github.com>
2024-05-25 17:24:15 +0200

    Update test_functions.py

--- molpal/test_functions.py
+++ molpal/test_functions.py
@@ -54,53 +54,24 @@ def selection_fct(csv_all, csv_allselected, output_csv):
     return new_df1.to_csv(output_csv, index=False)
 
 
-def molpal_run(model, confid, metrics, init, batches, max_iter, k, BETA, dataset, top_x_perc):
+
+def frac_top_x(top_x_perc, dataset, csv_selectedsmiles):
     """
-    Runs the molpal tool with specified parameters, processes the results, and evaluates the fraction of top X% molecules found.
+    Calculates the fraction of molecules from the selected smiles that are in the top X% of the dataset based on scores.
 
     Parameters:
-    - model (str): The model to be used by molpal.
-    - confid (str): The confidence method to be used.
-    - metrics (str): The metric for evaluation.
-    - init (int): Initial size of the dataset.
-    - batches (int): Batch size for iterations.
-    - max_iter (int): Maximum number of iterations.
-    - k (int): Number of top samples to select.
-    - BETA (float): Beta parameter for model configuration.
-    - dataset (int): Dataset identifier used to specify the dataset file.
-    - top_x_perc (float): The top X percentage of molecules to consider.
+    - top_x_perc (float): The top X percent of the dataset to consider (e.g., 0.1 for top 10%).
+    - dataset (int): Dataset identifier used in the file name.
+    - csv_selectedsmiles (str): Path to the CSV file containing the selected smiles.
+
+    Returns:
+    - float: The fraction of top X% smiles found in the selected smiles.
     """
-
-    command = [
-        "molpal", "run",
-        "--write-intermediate", "--write-final", "--retrain-from-scratch",
-        "--library", f"/content/molpal/data/Enamine{dataset}k_scores.csv.gz",
-        "-o", "lookup",
-        "--objective-config", "/content/molpal/examples/objective/Enamine10k_lookup.ini",
-        "--model", model,
-        "--conf-method", confid,
-        "--metric", metrics,
-        "--init-size", str(init),
-        "--batch-size", str(batches),
-        "--max-iters", str(max_iter),
-        "--fps", "/content/molpal/folder_output/fps_file.h5",
-        "--output-dir", "run_output",
-        "-k", str(k),
-        "--beta", str(BETA)
-    ]
-
-    subprocess.run(command)
-
-    output_filename_csv = f"/content/molpal/folder_output/run_output/all_explored_final_{model}_{metrics}_{init}_{batches}_{max_iter}_beta_{BETA}.csv"
-    os.rename("/content/molpal/folder_output/run_output/all_explored_final.csv", output_filename_csv)
-
-    output_folder_name = f"/content/molpal/folder_output/run_output_{model}_{metrics}_{init}_{batches}_{max_iter}_beta_{BETA}"
-    os.rename("/content/molpal/folder_output/run_output", output_folder_name)
 
     file_path = f'/content/molpal/data/Enamine{dataset}k_scores.csv.gz'
     df = pd.read_csv(file_path)
 
-    df_sorted = df.sort_values(by='score', ascending=True)
+    df_sorted = df.sort_values(by='score', ascending=False)
 
     percentile_index = int(len(df_sorted) * top_x_perc)
 
@@ -109,8 +80,8 @@ def molpal_run(model, confid, metrics, init, batches, max_iter, k, BETA, dataset
     output_file_path = f'/content/molpal/data/Top_{top_x_perc}_Enamine10k_scores.csv'
     top_x_smiles.to_csv(output_file_path, index=False)
 
-    df_found = pd.read_csv(f"/content/molpal/folder_output/run_output_{model}_{metrics}_{init}_{batches}_{max_iter}_beta_{BETA}/all_explored_final_{model}_{metrics}_{init}_{batches}_{max_iter}_beta_{BETA}.csv")
-    df_top_x = pd.read_csv(output_file_path)
+    df_found = pd.read_csv(csv_selectedsmiles)
+    df_top_x = pd.read_csv(f'/content/molpal/data/Top_{top_x_perc}_Enamine10k_scores.csv')
 
     smiles_found = set(df_found['smiles'])
     smiles_top_x = set(df_top_x['smiles'])
@@ -120,14 +91,133 @@ def molpal_run(model, confid, metrics, init, batches, max_iter, k, BETA, dataset
     num_common_smiles = len(common_smiles)
     print("Fraction of top", top_x_perc * 100, " % of smiles found:", num_common_smiles * 100 / len(smiles_top_x), "%")
 
+    return num_common_smiles * 100 / len(smiles_top_x)
 
-    process_and_merge_csvs(f"/content/molpal/folder_output/run_output_{model}_{metrics}_{init}_{batches}_{max_iter}_beta_{BETA}/all_explored_final_{model}_{metrics}_{init}_{batches}_{max_iter}_beta_{BETA}.csv",
-                           f"/content/molpal/folder_output/run_output_{model}_{metrics}_{init}_{batches}_{max_iter}_beta_{BETA}/all_ucb.csv",
-                           f"/content/molpal/folder_output/run_output_{model}_{metrics}_{init}_{batches}_{max_iter}_beta_{BETA}/all_std.csv",
-                           f"/content/molpal/folder_output/run_output_{model}_{metrics}_{init}_{batches}_{max_iter}_beta_{BETA}/all_data"
-                           )
 
-    selection_fct(f"/content/molpal/folder_output/run_output_{model}_{metrics}_{init}_{batches}_{max_iter}_beta_{BETA}/all_data",
-                  f"/content/molpal/folder_output/run_output_{model}_{metrics}_{init}_{batches}_{max_iter}_beta_{BETA}/all_explored_final_{model}_{metrics}_{init}_{batches}_{max_iter}_beta_{BETA}.csv",
-                  f"/content/molpal/folder_output/run_output_{model}_{metrics}_{init}_{batches}_{max_iter}_beta_{BETA}/all_selected_data"
-    )
+def experiment_top_ef(model, confid, metrics, init, batches, max_iter, dataset, top_x_perc, k, BETA):
+    """
+    Runs the molpal tool with specified and random metrics, evaluates the fraction of top X% molecules found, and calculates the Enrichment Factor (EF).
+
+    Parameters: all listed above molpal_run2
+
+    Functionality:
+    1. Runs the molpal tool with specified parameters using the provided metric.
+    2. Runs the molpal tool with specified parameters using a random metric.
+    3. Reads the output files from both runs.
+    4. Calculates the fraction of top X% molecules found in each run.
+    5. Prints and returns the fraction for both runs and the Enrichment Factor (EF).
+
+    Outputs:
+    - Prints the fraction of top X% molecules found for both specified and random metrics.
+    - Prints the calculated Enrichment Factor (EF).
+    - Returns the fraction for both runs and the EF.
+
+    """
+
+    molpal_run2(model, confid, metrics, init, batches, max_iter, k, BETA)
+
+    molpal_run_random2(model, confid, init, batches, max_iter, k, BETA)
+
+    csv_file_random = f'/content/molpal/folder_output/run_output_{model}_random_{init}_{batches}_{max_iter}_beta_{BETA}/all_explored_final_{model}_random_{init}_{batches}_{max_iter}_beta_{BETA}.csv'
+    csv_file = f'/content/molpal/folder_output/run_output_{model}_{metrics}_{init}_{batches}_{max_iter}_beta_{BETA}/all_explored_final_{model}_{metrics}_{init}_{batches}_{max_iter}_beta_{BETA}.csv'
+
+    frac = frac_top_x(top_x_perc, dataset, csv_file)
+
+    frac_random = frac_top_x(top_x_perc, dataset, csv_file_random)
+
+    print("The percentage of molecules recovered from the top", top_x_perc, "% is", frac, "% for", metrics, "and", frac_random, "% for random")
+    EF = frac / frac_random
+
+    print("The Enrichment Factor (EF) was calculated to be", EF)
+
+    return frac, EF
+
+def experiment_multirun(model, confid, metrics, init, batches, max_iter, dataset, top_x_perc, k, BETA, n):
+    """
+    Executes multiple runs of molpal with specified and random metrics, evaluates the fraction of top X% molecules found, and calculates the Enrichment Factor (EF).
+
+    Parameters:
+    - model (str): The model to be used by molpal.
+    - confid (str): The confidence method to be used.
+    - metrics (str): The metric for evaluation.
+    - init (int): Initial size of the dataset.
+    - batches (int): Batch size for iterations.
+    - max_iter (int): Maximum number of iterations.
+    - dataset (int): Dataset identifier used to specify the dataset file.
+    - top_x_perc (float): The top X percentage of molecules to consider.
+    - k (int): Number of top samples to select.
+    - BETA (float): Beta parameter for model configuration.
+    - n (int): Run index for identifying multiple runs.
+
+    Functionality:
+    1. Runs molpal with specified metrics and saves the output with the run index.
+    2. Runs molpal with random metrics and saves the output with the run index.
+    3. Reads the output files from both runs.
+    4. Calculates the fraction of top X% molecules found in each run.
+    5. Prints and returns the fraction for both runs and the Enrichment Factor (EF).
+
+    Outputs:
+    - Prints the fraction of top X% molecules found for both specified and random metrics.
+    - Prints the calculated Enrichment Factor (EF).
+    - Returns the fraction for both runs and the EF.
+    """
+
+    # Run molpal with the specified metrics
+    molpal_run3(model, confid, metrics, init, batches, max_iter, k, BETA, n)
+
+    # Run molpal with random metrics
+    molpal_run_random3(model, confid, init, batches, max_iter, k, BETA, n)
+
+    # Define file paths for the output CSV files
+    csv_file_random = f'/content/molpal/folder_output/run_output_{model}_random_{init}_{batches}_{max_iter}_beta_{BETA}_run{n}/all_explored_final_{model}_random_{init}_{batches}_{max_iter}_beta_{BETA}_run{n}.csv'
+    csv_file = f'/content/molpal/folder_output/run_output_{model}_{metrics}_{init}_{batches}_{max_iter}_beta_{BETA}_run{n}/all_explored_final_{model}_{metrics}_{init}_{batches}_{max_iter}_beta_{BETA}_run{n}.csv'
+
+    # Calculate the fraction of top X% molecules found for the specified metrics
+    frac = frac_top_x(top_x_perc, dataset, csv_file)
+
+    # Calculate the fraction of top X% molecules found for the random metrics
+    frac_random = frac_top_x(top_x_perc, dataset, csv_file_random)
+
+    # Print the results
+    print("The percentage of molecules recovered from the top", top_x_perc, "% is", frac, "% for", metrics, "and", frac_random, "% for random")
+
+    # Calculate the Enrichment Factor (EF)
+    EF = frac / frac_random
+
+    # Print the Enrichment Factor (EF)
+    print("The Enrichment Factor (EF) was calculated to be", EF)
+
+    # Return the fractions and the Enrichment Factor (EF)
+    return frac, EF
+
+def experiment_multirun2(model, confid, metrics, init, batches, max_iter, dataset, top_x_perc, k, BETA, n):
+    """
+    Executes a single run of molpal with specified metrics and evaluates the fraction of top X% molecules found.
+
+    Parameters: same as experiment_multirun
+
+    Functionality:
+    1. Runs molpal with the specified metrics and saves the output with the run index.
+    2. Reads the output file from the run.
+    3. Calculates the fraction of top X% molecules found.
+    4. Prints and returns the fraction of top X% molecules found.
+
+    Outputs:
+    - Prints the fraction of top X% molecules found.
+    - Returns the fraction of top X% molecules found.
+    """
+
+    # Run molpal with the specified metrics
+    molpal_run3(model, confid, metrics, init, batches, max_iter, k, BETA, n)
+
+    # Define the file path for the output CSV file
+    csv_file = f'/content/molpal/folder_output/run_output_{model}_{metrics}_{init}_{batches}_{max_iter}_beta_{BETA}_run{n}/all_explored_final_{model}_{metrics}_{init}_{batches}_{max_iter}_beta_{BETA}_run{n}.csv'
+
+    # Calculate the fraction of top X% molecules found
+    frac = frac_top_x(top_x_perc, dataset, csv_file)
+
+    # Print the result
+    print("The percentage of molecules recovered from the top", top_x_perc * 100, "% is", frac, "% for", metrics)
+
+    # Return the fraction
+    return frac
